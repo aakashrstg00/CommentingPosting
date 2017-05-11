e9aa33ee6c22b23e92482bc5c6dec95d3310d714
Aakash Rastogi <1998aakashrastogi@gmail.com>
2017-05-11 23:48:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 var urls = {
     getPosts: "https://raw.githubusercontent.com/aakashrstg00/CommentingPosting/master/temp.json"
-    , postComment: "lol"
-    , deleteComment: "lol2"
+    , postComment: " *insert server link to handle new comments* "
+    , deleteComment: " *insert server link to handle deleted comments* "
+    , postPost: " *insert server link to handle updated posts* "
 }
